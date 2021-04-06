2fdaca024c0dc11c297915d5f0da8248b6d4f3bf
Max <max.magill@yahoo.co.uk>
2021-04-06 12:26:20 +0100

    add first page

---
+++ DnDApp/Views/Support Views/ProfTable.swift
@@ -0,0 +1,20 @@
+//
+//  ProfTable.swift
+//  DnDApp
+//
+//  Created by Max Magill on 30/03/2021.
+//
+
+import SwiftUI
+
+struct ProfTable: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfTable_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfTable()
+    }
+}
