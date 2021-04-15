3490ea6c1b66e0176d5c53474819139aba831be9
Max <max.magill@yahoo.co.uk>
2021-04-15 13:33:39 +0100

    add currency

---
+++ DnDApp/Views/Support Views/CurrencyBox.swift
@@ -0,0 +1,68 @@
+import SwiftUI
+import Combine
+
+struct CurrencyBox: View {
+    @State var currency: String
+    var currencyName: String
+    @ObservedObject var charVM: CharacterViewModel
+
+    var body: some View {
+        ZStack(alignment: .center){
+            Color.flatDarkCardBackground
+                .frame(width: 60, height: 65, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(radius: 20)
+            
+            VStack {
+                TextField("0", text: $currency)
+                    // santise input
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(currency)) { newValue in
+                        let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            self.currency = filtered
+                        }
+                    }
+                    .onChange(of: currency, perform: { newValue in
+                        currencyChange(currencyChanging: currencyName, currency: currency)
+                    })
+                    .fixedSize()
+                    .foregroundColor(.white)
+                    .font(Font.custom("OpenSans-Bold", size: 24))
+                    .offset(y: 5)
+                
+                Text(currencyName)
+                    .foregroundColor(.white)
+                    .font(.subheadline)
+            }
+        }
+    }
+    
+    func currencyChange(currencyChanging: String, currency: String){
+        switch(currencyChanging){
+            case("CP"):
+                charVM.character.cp = Int(currency) ?? 0
+                charVM.updateCharacter()
+            case("SP"):
+                charVM.character.sp = Int(currency) ?? 0
+                charVM.updateCharacter()
+            case("GP"):
+                charVM.character.gp = Int(currency) ?? 0
+                charVM.updateCharacter()
+            case("EP"):
+                charVM.character.ep = Int(currency) ?? 0
+                charVM.updateCharacter()
+            case("PP"):
+                charVM.character.pp = Int(currency) ?? 0
+                charVM.updateCharacter()
+            default:
+                break
+        }
+    }
+}
+
+struct CurrencyBox_Previews: PreviewProvider {
+    static var previews: some View {
+        CurrencyBox(currency: "45", currencyName: "GP", charVM: CharacterViewModel())
+    }
+}
